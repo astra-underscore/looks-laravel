43d505300037bd0d5406993495869408bd287adb
Astra López <astra.lopez.zagal@gmail.com>
2025-03-31 20:44:43 -0300

    Initial commit

---
+++ resources/js/Components/InputLabel.jsx
@@ -0,0 +1,18 @@
+export default function InputLabel({
+    value,
+    className = '',
+    children,
+    ...props
+}) {
+    return (
+        <label
+            {...props}
+            className={
+                `label ` +
+                className
+            }
+        >
+            <span className="label-text">{value ? value : children}</span>
+        </label>
+    );
+}
